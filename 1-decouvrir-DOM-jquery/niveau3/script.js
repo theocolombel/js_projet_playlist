a249a04cce312ad8b8169f3a0209cbdeddf2f9cb
theocolombel <59018739+theocolombel@users.noreply.github.com>
2020-03-19 15:14:46 +0100

    exo 3

--- 1-decouvrir-DOM-jquery/niveau3/script.js
+++ 1-decouvrir-DOM-jquery/niveau3/script.js
@@ -3,13 +3,27 @@ var listOfMovie = [];
 $(document).ready(function(){
 
       // completer le code ici
+      $('#refresh').click(function() {
+
+        $('#list ul').html('');
+        
+        $.get('./playlist.txt', function(playlist) {
+           
+           playlist = splitFile(playlist);
+
+           listOfMovie.forEach(e => {
+             $('#list ul').append(htmlDivElement(e));
+           });
+        });
+  
+     });
 
 });
 
 
 function htmlDivElement(movie){
 
-  var html = "";
+  var html = "<div class='divFilm'><div class='divIndex'>"+movie.index+"</div><div class='divTitle'>"+movie.name+" durée : "+movie.length+"</div>";
 // completer le code ici
   return html;
 
@@ -17,7 +31,11 @@ function htmlDivElement(movie){
 
 function createMovie(i, n, d){
 
-  var movie = {};
+  var movie = {
+    index: i,
+    name: n,
+    length: d
+  };
 
 // completer le code ici
   return movie;
@@ -25,6 +43,17 @@ function createMovie(i, n, d){
 
 function splitFile(data){
   // completer le code ici
+  data = data.split('\n');
+
+  data.forEach(e => {
+
+    movie = e.split(',');
+
+    
+    addMovie(createMovie(movie[0], movie[1], movie[2]));
+    
+  });
+  
 }
 
 function addMovie(m){
@@ -32,10 +61,3 @@ function addMovie(m){
   listOfMovie.push(m);
 
 }
-    
-
-
-
-
-
-
