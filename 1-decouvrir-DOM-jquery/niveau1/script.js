ede1b26b2960f2c6f5e67e54bc8e2f61b27fa7a6
theocolombel <59018739+theocolombel@users.noreply.github.com>
2020-03-19 14:43:57 +0100

    Niveau 1

--- 1-decouvrir-DOM-jquery/niveau1/script.js
+++ 1-decouvrir-DOM-jquery/niveau1/script.js
@@ -1,1 +1,18 @@
+$(function(){
 
+
+    $('button').click(function(){
+
+        $.get('./playlist.txt',function(data){
+            console.log(data);
+            data = data.split("\n");
+            data.forEach(element => {
+                $('#list ul').append('<li>'+element+'</li>');
+            });
+        });
+
+
+    });
+
+
+});
