90cc386a6048ee2a9e18819885de0465fcff593a
theocolombel <59018739+theocolombel@users.noreply.github.com>
2020-03-23 09:36:09 +0100

    LVL 4

--- 1-decouvrir-DOM-jquery/niveau4/script.js
+++ 1-decouvrir-DOM-jquery/niveau4/script.js
@@ -1,36 +1,52 @@
 var listOfMovie = [];
 
 $(document).ready(function(){
-// A completer
+  $('#refresh').click(function(){                 
+    $('#list').html('');                         
+    listOfMovie = [];                               
+  
+  $.get('./playlist.txt', function(data){         
+      
+    splitFile(data).forEach(element => {addMovie(createMovie(element[0], element[1], element[2]));    
+    });
+    listOfMovie.forEach(element => {$('#list').append(htmlDivElement(element));              
+    createPlayCallback(element);
+  })
+  });
+});
 });
 
-
-
 function htmlDivElement(movie){
- // A completer
+  return '<div class="divFilm"><div class="divIndex">'+ movie.index + '</div><div class="divTitle">' + movie.name + '</div><div class="divLength">' + movie.length + '</div><div><button id="playButton'+ movie.index +'">Play</button></div></div>';
 }
 
 function createPlayCallback(movie){
-// A completer
+  $('#playButton'+ movie.index).on('click', function(){
+    console.log('Film en cour : ' + movie.name + ',  pour une durée de : ' + movie.length);
+  });
 }
  
-function createMovie(i, n, d){
-// A completer
+function createMovie(i, n, l){
+  var movie = {                                       
+    index : i,
+    name : n,
+    length : l
+  };
+
+return movie;
 }
 
 function splitFile(data){ 
-// A completer
+  data = data.split('\n');                            
+
+  $.each(data, function(index, value){                
+    data[index] = value.split(',');                   
+  });
+
+  return data;
 }
 
 function addMovie(m){
-
   listOfMovie.push(m);
-
 }
     
-
-
-
-
-
-
